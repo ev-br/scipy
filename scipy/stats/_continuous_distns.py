Fix unit tests, handle overflow behavior in expongauss.
* Overflow in the exp occured for large negative x values, causing
  nan return.  Fixed, although there is still a warning that overflow
  has occurred.  Is that worth turning off inside function?
* Add logpdf, sf functions
* Fix spacing in docstring.
* Added to _distr_params, note added in test_continuous_basic.py
  that things have to be added there.
* Added to dists in test_distributions.py, also some specific
  unit tests added.
* Left the explicit _argcheck in there, since it seems helpful
  to be explicit rather than relying on default.

scipy/stats/_continuous_distns.py
@@ -16,7 +16,7 @@ from scipy.special import (gammaln as gamln, gamma as gam, boxcox, boxcox1p,
 
 from numpy import (where, arange, putmask, ravel, sum, shape,
                    log, sqrt, exp, arctanh, tan, sin, arcsin, arctan,
-                   tanh, cos, cosh, sinh)
+                   tanh, cos, cosh, sinh, nan_to_num)
 
 from numpy import polyval, place, extract, any, asarray, nan, inf, pi
 
@@ -983,18 +983,22 @@ class expongauss_gen(rv_continuous):
     The probability density function for `expongauss` is::
 
         expongauss.pdf(x, lam, s) = 
-           lam/2 * exp(lam/2 * (lam*s**2-2*x)) * erfc((lam*s**2-x)/(sqrt(2)*s))
+           lam/2 * exp(lam/2 * (lam*s**2 - 2*x)) * erfc((lam*s**2 - x) / (sqrt(2)*s))
 
-    for ``lam > 0, s > 0``.
-
+    for ``lam > 0, s > 0``.  It is the sum of a normally and exponentially
+    distributed variable.
 
     The two shape parameters for `expongauss` (``lam`` and ``s``) must
     be set explicitly.
+
     .. versionadded:: 0.16.0
 
     %(example)s
 
     """
+    def _argcheck(self, lam, s):
+        return (lam > 0) & (s > 0)
+    
     def _rvs(self, lam, s):
         expval = self._random_state.standard_exponential(self._size) / lam
         gval = s * self._random_state.standard_normal(self._size)
@@ -1003,14 +1007,32 @@ class expongauss_gen(rv_continuous):
     def _pdf(self, x, lam, s):
         ls2 = lam * s * s
         exparg = 0.5 * lam * (ls2 - 2 * x)
-        erfcarg = (ls2 - x) / (sqrt(2) * s)
-        return 0.5 * lam * exp(exparg) * erfc(erfcarg)
+        erfcval = (ls2 - x) / (sqrt(2) * s)
+        # The use of nan_to_num from numpy is a bit of an ugly kludge
+        # for dealing with overflow in exp(exparg).  The properties of
+        # erfc guarantee the product is defined, but python doesn't
+        # understand this and ends up with 0.5 * lam * inf * 0 = nan.
+        # nan_to_num takes that inf and turns it into the largest
+        # floating point number, which solves the problem.
+        return 0.5 * lam * (nan_to_num(exp(exparg)) * erfc(erfcval))
 
+    def _logpdf(self, x, lam, s):
+        ls2 = lam * s * s
+        exparg = 0.5 * lam * (ls2 - 2 * x)
+        erfcval = (ls2 - x) / (sqrt(2) * s)
+        return exparg + log(0.5 * lam * erfc(erfcval))
+    
     def _cdf(self, x, lam, s):
         u = lam * x
         v = lam * s
-        exparg = -u + 0.5 * v * v
-        return special.ndtr(x / s) - exp(exparg) * special.ndtr(x / s - v)
+        expval = nan_to_num(exp(-u + 0.5 * v * v))
+        return special.ndtr(x / s) - expval * special.ndtr(x / s - v)
+
+    def _sf(self, x, lam, s):
+        u = lam * x
+        v = lam * s
+        expval = nan_to_num(exp(-u + 0.5 * v * v))
+        return special.ndtr(-x / s) + expval * special.ndtr(x / s - v)
 
     def _stats(self, lam, s):
         invlam = 1.0 / lam

scipy/stats/tests/test_distributions.py
@@ -33,8 +33,8 @@ DOCSTRINGS_STRIPPED = sys.flags.optimize > 1
 dists = ['uniform','norm','lognorm','expon','beta',
          'powerlaw','bradford','burr','fisk','cauchy','halfcauchy',
          'foldcauchy','gamma','gengamma','loggamma',
-         'alpha','anglit','arcsine','betaprime',
-         'dgamma','exponweib','exponpow','frechet_l','frechet_r',
+         'alpha','anglit','arcsine','betaprime','dgamma',
+         'expongauss', 'exponweib','exponpow','frechet_l','frechet_r',
          'gilbrat','f','ncf','chi2','chi','nakagami','genpareto',
          'genextreme','genhalflogistic','pareto','lomax','halfnorm',
          'halflogistic','fatiguelife','foldnorm','ncx2','t','nct',
@@ -774,6 +774,21 @@ class TestExpon(TestCase):
     def test_tail(self):  # Regression test for ticket 807
         assert_equal(stats.expon.cdf(1e-18), 1e-18)
         assert_equal(stats.expon.isf(stats.expon.sf(40)), 40)
+
+        
+class TestExponGauss(TestCase):
+    def test_erfc0(self):
+        # Testing the special case where the argument to erfc is zero
+        #  (so x = lambda * sigma^2), where the value simplifies to
+        #  lambda / 2 * exp(-lambda * x / 2)
+        assert_almost_equal(stats.expongauss.pdf(1, 1, 1),
+                            0.5 * np.exp(-0.5))
+        assert_almost_equal(stats.expongauss.pdf(2*0.1*0.1, 2, 0.1),
+                            0.5 * 2 * np.exp(-0.5 * 2 * 0.1 * 0.1 * 2))
+
+    def test_veryneg_x(self):
+        # Test for large negative x where the exponent overflows
+        assert_almost_equal(stats.expongauss.pdf(-900, 1, 1), 0.0)
 
 
 class TestGenExpon(TestCase):