40b52a20e3a40e9d8a36f7e0bd689d925dd0f391
Evgeni Burovski <evgeny.burovskiy@gmail.com>
2024-07-21 16:04:51 +0300

    MAINT: interpolate" move fpback wrap from Cython to manual

--- scipy/interpolate/tests/test_bsplines.py
+++ scipy/interpolate/tests/test_bsplines.py
@@ -32,6 +32,7 @@ from scipy.interpolate._ndbspline import make_ndbspl
 from scipy.interpolate import _dfitpack as dfitpack
 from scipy.interpolate import _bsplines as _b
 from scipy.interpolate import _bspl
+from scipy.interpolate import _dierckx
 
 
 class TestBSpline:
@@ -1960,7 +1961,7 @@ class TestGivensQR:
 
         # sign changes are consistent between Q and R:
         c_full = sl.solve(r, qTy)
-        c_banded = _bspl._fpback(R.a, R.nc, y_)
+        c_banded = _dierckx.fpback(R.a, R.nc, y_)
         assert_allclose(c_full, c_banded[:, 0], atol=5e-13)
 
     def test_py_vs_compiled(self):
@@ -2015,7 +2016,7 @@ class TestGivensQR:
         _bspl._qr_reduce(A, offset, nc, y)
 
         c = fpback(R, y)
-        cc = _bspl._fpback(A, nc, y)
+        cc = _dierckx.fpback(A, nc, y)
 
         assert_allclose(cc, c, atol=1e-14)
 
